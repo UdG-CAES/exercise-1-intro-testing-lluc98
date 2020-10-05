5e6e3862c7de8b12c391307cf8adc36de306d903
Lluc Domenech <u1948278@campus.udg.edu>
2020-10-05 17:55:14 +0200

    fix unit tests

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -7,26 +7,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExerciseIntroTestingTest {
 
     @Test
-    public void testFindLast(){
-        final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5}, 2);
+    public void testFindLast() {
+        final int result = ExerciseIntroTesting.findLast(new int[]{2, 3, 5}, 2);
         assertEquals(result, 0);
     }
 
     @Test
-    public void testLastZero(){
-        final int result = ExerciseIntroTesting.lastZero(new int []{0, 1, 0});
+    public void testLastZero() {
+        final int result = ExerciseIntroTesting.lastZero(new int[]{0, 1, 0});
         assertEquals(result, 2);
     }
 
     @Test
-    public void testCountPositive(){
-        final int result = ExerciseIntroTesting.countPositive(new int []{-4, 2, 0, 2});
+    public void testCountPositive() {
+        final int result = ExerciseIntroTesting.countPositive(new int[]{-4, 2, 0, 2});
         assertEquals(result, 2);
     }
 
     @Test
-    public void testOddOrPos(){
-        final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
+    public void testOddOrPos() {
+        final int result = ExerciseIntroTesting.oddOrPos(new int[]{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
+    }
+
+    /*NEW TESTS*/
+
+    @Test
+    public void testFindLast2() {
+        final int result = ExerciseIntroTesting.findLast(new int[]{2, 3, 5}, 2);
+        assertEquals(result, -1);
+    }
+
+    @Test
+    public void testLastZero2() {
+        final int result = ExerciseIntroTesting.lastZero(new int[]{0, 1, 0});
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void testCountPositive2() {
+        final int result = ExerciseIntroTesting.countPositive(new int[]{-4, 2, 0, 2});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testOddOrPos2() {
+        final int result = ExerciseIntroTesting.oddOrPos(new int[]{-3, -2, 0, 1, 4});
+        assertEquals(result, 2);
     }
 }
